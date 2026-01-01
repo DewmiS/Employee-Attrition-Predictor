f4eae1ea52ef64a440ec3e5358104aedb2b30e10
DewmiS <dewmisamadhipathiranage@gmail.com>
2026-01-01 16:29:29 +0530

    Add sidebar

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ import pandas as pd
 from streamlit.source_util import page_icon_and_name
 
 
+def add_sidebar():
+    st.sidebar.header('User Inputs')
+
+
 def main():
     st.set_page_config(
         page_title = "Employee Attrition Predictor",
@@ -12,8 +16,18 @@ def main():
         initial_sidebar_state = "expanded",
     )
 
-    st.write("# Employee Attrition Predictor")
+    add_sidebar()
 
+    with st.container():
+        st.title("Employee Attrition Predictor")
+        st.write("Welcome to the Employee Attrition Predictor! This tool is here to help you explore employee data and gain insights that support better workplace decisions.Simply provide the details, and let the system assist you in understanding attrition patterns with ease.")
+
+    col1, col2 = st.columns([4,1])
+
+    with col1:
+        st.write("This is column 1")
+    with col2:
+        st.write("This is column 2")
 
 if __name__ == "__main__":
     main()
